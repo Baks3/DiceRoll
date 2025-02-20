24170a673aaf022e3ff0e714d09d379743ca6736
tmabizela023 <tmabizela023@student.wethinkcode.co.za>
2025-02-20 10:09:40 +0200

    improved functionality

--- 109.dicestimulator.py
+++ 109.dicestimulator.py
@@ -41,5 +41,7 @@ while x == "y":
         print("| O    O |")
         print("| O    O |")
         print("----------")
-    x = input("Press y to roll again")
+    x = input("Press 'y' to roll again or any other key to exit: ")
+
+print("Thank you for using the dice simulator!")
     
