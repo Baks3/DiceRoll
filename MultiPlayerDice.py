ca3e20ccffffd7fd9324ae786c246ef2a58b8dd6
tmabizela023 <tmabizela023@student.wethinkcode.co.za>
2025-02-21 16:29:15 +0200

    added a feature where users can enter their names and the program references them by their names instead of player1, 2 or 3

--- MultiPlayerDice.py
+++ MultiPlayerDice.py
@@ -5,15 +5,18 @@ num_players = int(input("Enter the number of players: "))
 rounds = int(input("Enter the number of rounds: "))
 
 scores = []
-for _ in range(num_players):
+player_names = []
+for num in range(num_players):
     scores.append(0)
+    player = input(f"Player {num + 1} enter your name: ")
+    player_names.append(player)
 
 print(scores)
 
 for round_number in range(1, rounds + 1):
     print(f"\nRound {round_number}")
     for player in range(num_players):
-        input(f"Player {player + 1}, press Enter to roll the dice...")
+        input(f"{player_names[player]}, press Enter to roll the dice...")
         number = random.randint(1, 6)
         print(f"You rolled a {number}\n")
         scores[player] += number
@@ -21,10 +24,10 @@ for round_number in range(1, rounds + 1):
 
 print("\nFinal Scores:")
 for player in range(num_players):
-    print(f"Player {player + 1}: {scores[player]}")
+    print(f"{player_names[player]}: {scores[player]}")
 
 high_score = max(scores)
-winner = scores.index(high_score) + 1
-print(f"\nCongratulations, Player {winner} wins! With a score of {high_score}")
+winner = scores.index(high_score)
+print(f"\nCongratulations, {player_names[winner]} wins! With a score of {high_score}")
 
 
