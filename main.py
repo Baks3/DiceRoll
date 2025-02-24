da3cd7af860bc35e1d67881028b37e2229c3859b
tmabizela023 <tmabizela023@student.wethinkcode.co.za>
2025-02-24 07:22:19 +0200

    fixed my main.py file

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 import MultiPlayerDice as MPD
 
-# def main():
-#     print("Welcome to the Dice Simulator!")
-    
-#     player_names = MPD.get_names()
-#     rounds = MPD.get_rounds()
-
-#     scores = [0] * len(player_names)
-
-#     MPD.roll_dice(rounds, player_names, scores)
-#     MPD.final_scores(player_names, scores)
-
-# if __name__ == "__main__":
-#     main()
-
 def main():
     player_names = MPD.get_names()
     rounds = MPD.get_rounds()
